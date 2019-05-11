82dcb73b25a26d2d4cf34dac12eb4821d1396bd8
karamalesa1980 <karamalesa@gmail.com>
2019-05-11 16:09:57 +0200

    lesson 33.2 finished

--- public/app.js
+++ public/app.js
@@ -13,5 +13,9 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('вы выбрали' + id);
+  var key = 'product_' + id;
+
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 }
