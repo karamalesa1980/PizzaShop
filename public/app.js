ffd69871b826644445f6ad6ac950491ba6717619
karamalesa1980 <karamalesa@gmail.com>
2019-05-11 19:06:49 +0200

    add cart_get_number_of_items() function

--- public/app.js
+++ public/app.js
@@ -18,4 +18,25 @@ function add_to_cart(id)
   var x = window.localStorage.getItem(key);
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
+
+  alert('Items in your cart: ' + cart_get_number_of_items());
+}
+
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+
+  for (var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение
+
+    if(key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1;
+    }
+  }
+
+  return cnt;
 }
