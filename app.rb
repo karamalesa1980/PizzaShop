ee37d75f654be47828b0508d2cbef91935a352f1
karamalesa1980 <karamalesa@gmail.com>
2019-05-13 23:36:42 +0200

    fixed calculate all orders

--- app.rb
+++ app.rb
@@ -9,20 +9,78 @@ require 'sqlite3'
 
 set :database, {adapter: "sqlite3", database: "pizzashop.sqlite3"}
 
-class Product < ActiveRecord::Base
-  
-end 
 
-get '/' do
-	@products = Product.all
-	erb :index
+class Product < ActiveRecord::Base
 end
 
+class Order < ActiveRecord::Base
+end
 
-get '/about' do
-	erb :about
-end	
+get '/' do
+  erb :index
+end
+
+get '/products' do
+  @products = Product.all
+  erb :products
+end
+
+# post '/products' do
+#   :cart
+# end
+
+get '/cart' do
+  erb :cart
+end
 
 post '/cart' do
-	erb :cart
-end	
+
+  @orders_input = params[:orders]
+
+  @items = parse_orders_input @orders_input
+
+  # если корзина пустая
+  if @items.length == 0
+    return erb "В корзине нет товаров"
+  end
+
+  # выводим список продуктов в корзине
+  @items.each do |item|
+    # id, cnt
+    item[0] = Product.find(item[0])
+  end
+
+  erb :cart
+end
+
+# Parse orders line:
+def parse_orders_input orders_input
+
+  s1 = @orders_input.split(/,/)
+
+  arr = []
+
+  s1.each do |x|
+    s2 = x.split(/\=/)
+    s3 = s2[0].split(/_/)
+
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+    arr.push arr2
+  end
+
+  return arr
+end
+
+post '/place_order' do
+  @o = Order.create params[:order]
+
+  erb :order_placed
+end
+
+get '/manager' do
+  @order = Order.all
+  erb :manager
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_060150) do
+ActiveRecord::Schema.define(version: 2019_05_12_183538) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.text "orders_input"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
